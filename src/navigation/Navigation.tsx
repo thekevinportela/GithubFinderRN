65e0e297f4e9ca19aa0d4029c48df370915f684f
Kevin Portela <thekevinportela@gmail.com>
2022-11-18 18:56:37 -0500

    onboarding UI, firebase installed and app builds

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Home} from '../screens/Home';
+import {Onborading} from '../screens/Onborading';
 import {User} from '../screens/User';
 
 const Stack = createNativeStackNavigator();
@@ -8,7 +9,7 @@ const Stack = createNativeStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Onboarding">
         <Stack.Screen
           options={{headerShown: false}}
           name="Home"
@@ -22,6 +23,11 @@ const Navigation = () => {
           })}
           name="User"
           component={User}
+        />
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Onboarding"
+          component={Onborading}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,3 +2,5 @@ declare module '@env' {
   export const GITHUB_URL: string;
   export const GITHUB_TOKEN: string;
 }
+
+declare module 'react-native-onboarding-swiper';
